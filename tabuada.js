c7f9261d869a4c361bea2f3b0228405830ec71d8
jsribeiro2407 <js.ribeiro@discente.ufma.br>
2024-03-31 23:18:51 -0300

    versão inicial

---
+++ tabuada.js
@@ -0,0 +1,31 @@
+function tab(){
+    var num = window.document.getElementById('num')
+    var end = document.getElementById('end')
+
+    if (num.value.length == 0 || end.value.length == 0){
+        window.alert('[ERRO]')
+    } else{
+
+    var n = Number(num.value)
+    var e = Number(end.value)
+    var x = 0
+    var r = ''
+    var res = window.document.getElementById('resposta')
+    res.innerHTML = `A tabuada do ${n} é: <br>`
+    do{
+        r = n*x
+        res.innerHTML += `${x} X ${n} = ${r} <br>` 
+        x++
+
+    } while( x <=e)
+    
+    }
+}
+
+function botao(){
+    enviar.style.background = 'blue'
+}
+
+function saiu(){
+    enviar.style.background = '#0C70F2'
+}
